8edf0c6a38ffdf017a7c86dafd31d46a1d6277fc
ponny35 <patipolsaechan@gmail.com>
2016-10-07 01:26:34 +0700

    Upload x2 beta

---
+++ x2.c
@@ -0,0 +1,99 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+main()
+{
+    double a,b,c,ans1,ans2,i,g,p,fx=1;
+    int h;
+    printf("Input your a : ");
+    scanf("%lf",&a);
+    printf("Input your b : ");
+    scanf("%lf",&b);
+    printf("Input your c : ");
+    scanf("%lf",&c);
+    if((a==0&&b==0&&c==0)||(a==0&&b==0&&c!=0))
+    {
+        printf("X=Complex");
+    }
+    else if (a==0&&b!=0&&c!=0)
+    {
+        //bx+c=0 bx=-c x=-c/b
+        ans1=-c/b;
+        printf("X=%.3lf",ans1);
+    }
+    else if (a!=0)
+    {
+        if(b*b-4*a*c>0)
+        {
+            ans1=(-b+sqrt(b*b-4*a*c))/(2*a);
+            ans2=(-b-sqrt(b*b-4*a*c))/(2*a);
+            printf("X1=%.3lf \nX2=%.3lf \n",ans1,ans2);
+        }
+        else if (b*b-4*a*c==0)
+        {
+             ans1=-b/(2*a);
+             printf("X1=%.3lf \n",ans1);
+        }
+        else if (b*b-4*a*c<0)
+        {
+            printf("X=Imange Number\n");
+            int ans;
+            printf("Do you want to see answer ? \n1)Yes 2)No\nEnter your number : ");
+            scanf("%1d",&ans);
+            if(ans==1)
+            {
+                /*ans=fabs(b*b-4*a*c);
+                i=fabs(b*b-4*a*c);
+                printf("i= %d",i);
+                h=ans;
+                printf("ans= %d h= %d\n",ans,h);
+                fx=1;
+                for(g=30;g>0;g--)
+                {
+                    p=g*g;
+                    while(1)
+                    {
+                        if(ans>=p)
+                        {
+                            int w=p,newg=g;
+                            printf("I am here befor.%d fx=%d\n",newg,fx);
+                            if(h%w==0)
+                            {
+                                printf("I am here after.%d\n",newg);
+                                fx=fx*newg;
+                                h=h/w;
+                                printf("fx=%d\n",fx);
+                            }
+                        }
+                    break;
+                    }
+                }
+                printf("%d\n",h);
+                printf("%d\n",fx);
+                if(b==0)
+                {
+                    printf("X1=%dSqrt(%d)I\n",fx,h);
+                    printf("X2=-%dSqrt(%d)I\n",fx,h);
+                }
+                else if (b>0)
+                {
+
+                }
+                else if (b<0)
+                {
+
+                }
+                ans=0;
+                printf("Do you want to see another type of answer ? 1)Yes 2)No\nEnter your number : ");
+                scanf("%1d",&ans);*/
+                if(ans==1)
+                {
+                    i=fabs(b*b-4*a*c);
+                    ans1=(-b+sqrt(i))/(2*a);
+                    ans2=(-b-sqrt(i))/(2*a);
+                    printf("X1=%.3lfI \nX2=%.3lfI \n",ans1,ans2);
+                }
+            }
+        }
+    }
+}
