b430538ac3c37ab6f346dfe77c33d0bea115bb63
ponny35 <patipolsaechan@gmail.com>
2016-10-10 00:39:38 +0700

    Revert "Revert "prime 1.0.1""
    This reverts commit 4d0669252c22a6d0d554d8868b7f09dce560ae05.

--- prime check.c
+++ prime check.c
@@ -1,47 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 main ()
 {
     system("color A");
-    int num=9,n=2,w=0,i ;
+    int num,w=0,ab,ab2;
+    double anspow,anspow2,i=1;
     printf("Please input your number. : ");
     scanf("%d",&num);
-    for(i =2;i <=num-1;i++)
-        {
-            if(num%i<=0)
-                {
-                    w=5;
-                    break;
-                }
-        }
-    printf("%d is ",num);
-    //check 1or-1 -2 -3 ...
+    while(num>=anspow)
+    {
+        anspow=pow(i,2);
+        anspow2=pow(i+1,2);
+    }
+    ab=abs(num-anspow);
+    ab2=abs(num-anspow2);
+    if(ab2>ab)
+    {
+        for()
+    }
+    else
+    {
+        for
+    }
     if(num<2)
         {
-           printf("not prime.\n");
+           printf("%d is not prime.\n",num);
         }
     else
         {
         if(w<1)
             {
-                printf("prime.\n");
+                printf("%d is prime.\n",num);
             }
         else
             {
-                printf("not prime.\n");
-            }
-            printf("Factors: ");
-            for(i =1;i <=num;i++)
-            {
-                if(num%i==0)
+                printf("%d is not prime.\n",num);
+                printf("Factors: ");
+                for(i =1;i <=num;i++)
                 {
-                    printf("%d",i);
-                    if(i<num)
+                    if(num%i==0)
                     {
-                        printf(" ");
+                        printf("%d",i);
+                        if(i<num)
+                        {
+                            printf(" ");
+                        }
                     }
-                }
 
+                }
             }
         }
 }
